b9383ec16e5b17caa554c6ee090edac6c4bd5f4e
maldur94 <piotrbartkowiak94@gmail.com>
2023-05-14 18:00:35 +0200

    change app icon, add kebab menu with export option

--- app/src/main/java/com/maldur94/lirycsbar/component/LirycsBarAppBar.kt
+++ app/src/main/java/com/maldur94/lirycsbar/component/LirycsBarAppBar.kt
@@ -1,10 +1,20 @@
 package com.maldur94.lirycsbar.component
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.maldur94.lirycsbar.R
 import com.maldur94.lirycsbar.model.LirycsBarScreen
 import com.maldur94.lirycsbar.resources.Dimens
 
@@ -17,6 +27,47 @@ fun LirycsBarAppBar(currentScreen: LirycsBarScreen, navigateUp: () -> Unit) {
         navigationIcon = {
             IconButton(onClick = { navigateUp() }) {
                 Icon(Icons.Filled.ArrowBack, null)
+            }
+        },
+        actions = {
+            val context = LocalContext.current
+            var isKebabMenuExpanded by remember { mutableStateOf(false) }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentSize(Alignment.TopEnd)
+            ) {
+                IconButton(onClick = { isKebabMenuExpanded = !isKebabMenuExpanded }) {
+                    Icon(
+                        imageVector = Icons.Default.MoreVert,
+                        contentDescription = "More"
+                    )
+                }
+                DropdownMenu(
+                    expanded = isKebabMenuExpanded,
+                    onDismissRequest = { isKebabMenuExpanded = false }
+                ) {
+                    DropdownMenuItem(
+                        onClick = {
+                            Toast.makeText(context, R.string.exporting, Toast.LENGTH_SHORT).show()
+                        },
+                        content = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.ic_download),
+                                contentDescription = stringResource(
+                                    R.string.export_lirycs_into_csv_format
+                                )
+                            )
+                            Spacer(modifier = Modifier.width(width = 8.dp))
+                            Text(
+                                text = stringResource(R.string.export_lirycs_into_csv_format),
+                                style = MaterialTheme.typography.body2,
+                                fontWeight = FontWeight.Medium
+                            )
+                        }
+                    )
+                }
             }
         }
     )
